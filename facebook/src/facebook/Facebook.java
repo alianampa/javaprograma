6ffad0aab9f40afa5550d7b86da0acd3fde7fa12
aanampa08 <aliianampa2003@gmail.com>
2023-08-22 16:31:09 -0300

    Creacion de interfaz para el usuario

--- facebook/src/facebook/Facebook.java
+++ facebook/src/facebook/Facebook.java
@@ -1,8 +1,26 @@
 package facebook;
 
+import java.util.Scanner;
+
 public class Facebook {
     // Se solicita que el usuario ingrese los datos para el inicio de sesion
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Sistema sis= new Sistema();
+        
+        System.out.println("Facebook");
+        System.out.print("Ingresar usuario: ");
+        sis.usuario= sc.next();
+        System.out.print("Ingresar contraseña: ");
+        sis.contraseña= sc.next();
+        
+        if (sis.validar_ingreso()){
+            System.out.println("Felicidades "+ sis.usuario+" has ingresado al programa correctamente!");
+        }
+        else{
+            System.out.println("Lo sentimos, pero el usuario "+ sis.usuario +" no se encuentra en el sistema");
+        }
+        
     }
     
 }
