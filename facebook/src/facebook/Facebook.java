25bb9c4ef365fd8ca85b21b0884d31d346ccedb5
alianampa <115636009+alianampa@users.noreply.github.com>
2023-08-22 16:24:29 -0300

    Descripcion sobre lo que se requiere en Facebook.java

--- facebook/src/facebook/Facebook.java
+++ facebook/src/facebook/Facebook.java
@@ -1,7 +1,7 @@
 package facebook;
 
 public class Facebook {
-
+    // Se solicita que el usuario ingrese los datos para el inicio de sesion
     public static void main(String[] args) {
     }
     
